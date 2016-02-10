a2c02a3b008ccf6c5a5b3feaecd2014f57d6d8fd
Alexandr Karpov <AVKarpov@bssys.com>
2016-02-10 14:13:22 +0300

    stop activemq

--- sBuildHere.sh
+++ sBuildHere.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 alias build='JAVA_HOME=/usr/lib/jvm/java-7-oracle; mvn clean install -DskipTests=true package -P makeDistr,DefaultJarSignParameters,copyWars,!publishSDK,!convertClosures | tee -a ~/.build.log '
+
+activemq purge
+activemq stop
+
 build
